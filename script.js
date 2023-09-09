1954ca5ea5585b3b705bdbd44357dd4dfcf08a7e
Hero Aparre <heroaparre02@gmail.com>
2023-09-09 16:55:52 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let number = [0,1,2,3,4,5,6,7,8,9];
+
+
+if(document.getElementById("btn0").onclick){
+    document.getElementById("txtOutput").value = number[0];
+}else if(document.getElementById("btn1").onclick){
+    document.getElementById("txtOutput").value = number[1];
+}
